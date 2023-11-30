0ec5427414903428a5c52503b6d91cfe67fb40bb
Mahin Ahmed <mahindipto18@gmail.com>
2023-11-30 14:03:11 +0600

    added users delete and make hgr api

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
 
 //middleware
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+
     const servicesCollection = client.db('DigitalSculpDB').collection('services');
     const testimonialsCollection = client.db('DigitalSculpDB').collection('testimonials');
     const usersCollection = client.db('DigitalSculpDB').collection('users');
@@ -33,44 +33,80 @@ async function run() {
     //services
 
     app.get('/services', async (req, res) => {
-        const cursor = servicesCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-      })
+      const cursor = servicesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
     //testimonials
 
     app.get('/testimonials', async (req, res) => {
-        const cursor = testimonialsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-      })
+      const cursor = testimonialsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
 
     //User
 
     app.post('/users', async (req, res) => {
-        const user = req.body
-        const query = { email: user.email };
-        const existingUser = await usersCollection.findOne(query);
-        if (existingUser) {
-          return res.send({ message: "account already exist" });
-        }
-        const result = await usersCollection.insertOne(user)
-        res.send(result)
-      });
+      const user = req.body
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "account already exist" });
+      }
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    });
 
-      app.get('/users', async (req, res) => {
-        const cursor = usersCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-      })
+    app.get('/users', async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          userRole: 'hr'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    app.delete('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
   
+    app.get('/users/:email', async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-   
+
   }
 }
 run().catch(console.dir);
@@ -78,10 +114,10 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('DigitalSculp Server Running');
-  })
-  
-  
-  app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
-  })
+  res.send('DigitalSculp Server Running');
+})
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`)
+})
